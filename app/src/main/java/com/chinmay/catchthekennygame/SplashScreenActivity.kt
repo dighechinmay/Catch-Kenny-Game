9f1fe5e2e3bfcf5d8fd75540a2f04429a3c8fc60
chinmay <chinmay.dighe@emxdigital.com>
2020-05-09 11:53:57 +0530

    Splashscreen added with new button

--- app/src/main/java/com/chinmay/catchthekennygame/SplashScreenActivity.kt
+++ app/src/main/java/com/chinmay/catchthekennygame/SplashScreenActivity.kt
@@ -1,12 +1,21 @@
 package com.chinmay.catchthekennygame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+    }
+
+
+
+    fun startGame(view: View){
+        val intent = Intent(applicationContext,MainActivity::class.java)
+        startActivity(intent)
     }
 }
